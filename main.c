09f641418567fffa4e2ae98739d9e6081f9a0b2f
Aikan Harp <123391232+AikanH@users.noreply.github.com>
2023-02-08 15:26:39 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,39 @@
+ // Block letter project
+#include <stdio.h>
+
+typedef struct BLOCK_LETTER { 
+  char letter;
+  char block[5][4];
+} blockletter;
+
+void print_blockletter(blockletter bl);
+
+int main(void) {
+
+  blockletter a = {
+      'a',
+      {   
+        {' ', 'A', 'A', ' '},
+        {'A', ' ', ' ', 'A'},
+        {'A', 'A', 'A', 'A'},
+        {'A', ' ', ' ', 'A'},
+        {'A', ' ', ' ', 'A'}
+      }  
+  };
+  
+  print_blockletter(a);
+  
+
+  
+  return 0;
+}
+void print_blockletter(blockletter bl) {
+  
+  for (int row = 0; row <5; row++) {
+    for (int col = 0; col <4; col++) {
+   
+      printf("%c", bl.block[row][col]);
+      }
+    printf("\n");
+  }
+};
